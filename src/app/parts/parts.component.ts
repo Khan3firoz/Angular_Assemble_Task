5eb5764553b8769cc042cabf42ff503fcbb7b7a9
Firoz <khan3firoz@gmail.com>
2021-03-12 01:47:52 +0530

    added image dynamicaly

--- src/app/parts/parts.component.ts
+++ src/app/parts/parts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { url } from 'inspector';
 
 @Component({
   selector: 'app-parts',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parts.component.css']
 })
 export class PartsComponent implements OnInit {
+  // name='firoz khan'
+  // image="../../assets/img/parts/4.jpg"
+   public data = [
+    {name:'Sheet',image:'../../assets/img/parts/sheet.jpg'},
+    {name:'wheel',image:'../../assets/img/parts/wheel.jpg'},
+    {name:'handle',image:'../../assets/img/parts/handle.jpg'},
+    {name:'frame',image:'../../assets/img/parts/frame.jpg'},
+    {name:'frontwheel',image:'../../assets/img/parts/frontwheel.jpg'},
+    {name:'Other',image:'../../assets/img/parts/4.jpg'},
+    {name:'Other',image:'../../assets/img/parts/1.jpg'},
+    {name:'Other',image:'../../assets/img/parts/8.jpg'}
+  ]
 
   constructor() { }
 
